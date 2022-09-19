b4340b711013760cf9ed7e6591a96943e5575264
Emerson Marques <emerson.alexandre@venturus.org.br>
2022-09-19 15:48:05 -0300

    aula 2

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -2,6 +2,7 @@ import { View, Image, FlatList } from 'react-native';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { GameCard } from '../../components/GameCard';
+
 import { Heading } from '../../components/Heading';
 
 import { GAMES } from   '../../utils/games';
@@ -28,7 +29,7 @@ export function Home() {
               data={item}
             />
           )}
-          showsHorizontalScrollIndicator
+          showsHorizontalScrollIndicator={false}
           horizontal
           contentContainerStyle={styles.contentList}
         />
